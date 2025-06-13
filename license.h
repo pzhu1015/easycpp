33624af7400bbd240feeaf9e5b049996f6f5ce35
pengzhihu <pengzhihu1015@163.com>
2025-06-13 15:51:29 +0800

    license

--- license.h
+++ license.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <string.h>
+#include <stdio.h>
 #include <iostream>
 #include <string>
 #include <unordered_map>
@@ -21,30 +23,39 @@ static const std::unordered_map<LCC_EVENT_TYPE, std::string> Events = {
     {IDENTIFIERS_MISMATCH,             "已计算标识符与许可证中提供的标识符不匹配"},
 };
 
-static void Acquire()
+static void Acquire(const std::string &license_file, const std::string &module)
 {
     try
     {
+        std::cout << "验证许可文件: " << license_file << ", 模块: " << module << std::endl;
         LicenseInfo info;
-        LicenseLocation location = {LICENSE_PATH, "license.lic"};
-        auto result = acquire_license(nullptr, &location, &info);
+        LicenseLocation location;
+        location.license_data_type = LICENSE_PATH;
+        strcpy(location.licenseData, license_file.data());
+        CallerInformations caller;
+        auto result = acquire_license(&caller, &location, &info);
         if (result == LICENSE_OK)
         {
             std::cout << "许可证正常!!!" << std::endl;
             if (!info.linked_to_pc)
             { 
-                std::cout << "许可证中没有硬件签名。这是一个“演示”许可证，适用于所有电脑。" << std::endl;
-                std::cout << "要生成“单一电脑”许可证，请使用选项 -s 调用“发行许可证”，并使用之前获得的硬件标识符。" << std::endl;
+                std::cerr << "许可证中没有硬件签名。这是一个“演示”许可证，适用于所有电脑。" << std::endl;
+                std::cerr << "要生成“单一电脑”许可证，请使用选项 -s 调用“发行许可证”，并使用之前获得的硬件标识符。" << std::endl;
                 exit(0);
             }
             else
             {
+                if (module != caller.feature_name)
+                {
+                    std::cerr << "证书模块不匹配: " << module << std::endl;
+                    exit(0);
+                }
                 std::cout << "是否过期: " << info.has_expiry << std::endl;
                 std::cout << "到期时间: " << info.expiry_date << std::endl;
                 std::cout << "还剩时间: " << info.days_left << std::endl;
                 if (!info.has_expiry)
                 {
-                    std::cout << "许可证过期" << std::endl;
+                    std::cerr << "许可证过期" << std::endl;
                     exit(0);
                 }
             }
@@ -54,7 +65,7 @@ static void Acquire()
             size_t pc_id_sz = LCC_API_PC_IDENTIFIER_SIZE;
             char pc_identifier[pc_id_sz + 1];
             std::cout << "许可证错误：" << Events.at(result) << std::endl;
-            if (identify_pc(STRATEGY_DEFAULT, pc_identifier, &pc_id_sz, nullptr)) 
+            if (identify_pc(STRATEGY_DISK, pc_identifier, &pc_id_sz, nullptr)) 
             {
                 std::cout << "硬件签名为：[" << pc_identifier << "]" << std::endl;
             } 
