26ed7e868e800eff00757ecda35bb5d5fed0f0ed
pengzhihu <pengzhihu1015@163.com>
2025-07-11 20:48:13 +0800

    添加odb mysql value traits

---
+++ odb_value_traits.hxx
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <set>
+#include <json_serialize.h>
+#include <odb/mysql/traits.hxx>
+
+namespace odb 
+{
+namespace mysql
+{
+    template <>
+    class value_traits<std::set<std::string>, id_string> {
+    public:
+        using value_type = std::set<std::string>;
+        using query_type = value_type;
+        using image_type = details::buffer;
+
+        static void set_value(value_type& v, const image_type &i, size_t n, bool is_null) 
+        {
+            if (is_null)
+            {
+                v = value_type();
+                return;
+            }
+            v = serialize::JsonSerializer<value_type>::FromString(std::string(i.data(), n));
+        }
+        
+        static void set_image(image_type &i, size_t &n, bool& is_null, const value_type& v) 
+        {
+            is_null = false;
+            auto json = serialize::JsonSerializer<value_type>::ToString(v);
+            n = json.size();
+            if (i.capacity() < n) i.capacity(n);
+            std::memcpy(i.data(), json.c_str(), n);
+        }
+    };
+}
+}
